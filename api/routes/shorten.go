c85037106b1f48ede1a52e2977f9b8f9de81e693
Konstantin Orlov <genius4666@gmail.com>
2022-08-13 18:15:32 +0300

    part 4

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,10 +1,13 @@
 package routes
 
 import (
+	"os"
 	"time"
 
+	"github.com/genius321/UrlShorteningService/database"
 	"github.com/genius321/UrlShorteningService/helpers"
 	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 )
 
 type request struct {
@@ -32,6 +35,23 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	//implement rate limiting
 
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val, err : = r2.Get(databaseCtx, c.IP()).Result()
+	if err == redis.Nil{
+		_ = r2.Set(database.Ctx, c.IP, os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP().Result())
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error": "Rate limit exceeded",
+				"rate_limit_rest": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
+
 	//check if the input if an acutal URL
 
 	if !govalidator.IsURL(body.URL) {
@@ -51,4 +71,6 @@ func ShortenURL(c *fiber.Ctx) error {
 	//enforce https, SSL
 
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	r2.Decr(database.Ctx, c.IP())
 }
