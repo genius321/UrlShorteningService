374fef02ece8df2d2915ad078017027f8f31a5e0
Konstantin Orlov <genius4666@gmail.com>
2022-08-13 17:10:47 +0300

    part 2

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -15,3 +15,29 @@ type response struct {
 	XRateRemaining int           `json:"rate_limit"`
 	XRateLimitRest time.Duration `json:"rate_limit_reset"`
 }
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	//implement rate limiting
+
+	//check if the input if an acutal URL
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	//check for domain error
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON
+	}
+
+	//enforce https, SSL
+
+	body.URL = helpers.EnforceHTTP(body.URL)
+}
